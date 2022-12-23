12ee95abda0a19882214c7c2b33eafe75dd3bbdf
vishnu karthik <71687100+vissnukarthik@users.noreply.github.com>
2022-12-23 14:00:02 +0530

    template

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>karthis To do List</h1>
+      </header>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ src/components/ToList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ToList() {
+  return (
+    <div>ToList</div>
+  )
+}
+
+export default ToList
